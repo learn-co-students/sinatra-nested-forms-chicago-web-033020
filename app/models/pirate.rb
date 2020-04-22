863460aeba75512d664e07d79c73b90b9a9b007e
aseleidy <ash.etemadi@gmail.com>
2020-04-21 23:45:56 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height 
+
+  @@all = []
+
+  def initialize(specs)
+    @name = specs[:name]
+    @weight = specs[:weight]
+    @height = specs[:height]
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all
+  end 
 end
